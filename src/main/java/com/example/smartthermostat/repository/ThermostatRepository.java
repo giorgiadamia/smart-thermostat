21da542be00bcfbae451da7cac1f40c73cf5d72f
Giorgi <1giorgiadamia@gmail.com>
2023-04-10 14:35:06 +0400

    fixed delete thermostat method

--- src/main/java/com/example/smartthermostat/repository/ThermostatRepository.java
+++ src/main/java/com/example/smartthermostat/repository/ThermostatRepository.java
@@ -2,6 +2,8 @@ package com.example.smartthermostat.repository;
 
 import com.example.smartthermostat.model.domain.Thermostat;
 import lombok.NonNull;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,8 @@ import java.util.List;
 @Repository
 public interface ThermostatRepository extends CrudRepository<Thermostat, Long> {
     @NonNull List<Thermostat> findAll();
+
+    @Modifying
+    @Query(value = "DELETE FROM thermostats WHERE id = ?1" , nativeQuery = true)
+    void deleteById(@NonNull Long id);
 }
